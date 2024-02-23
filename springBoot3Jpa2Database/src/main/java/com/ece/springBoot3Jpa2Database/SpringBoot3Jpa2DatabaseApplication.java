9838898ebfe94a90534cff7a40899c0f07cd7727
NagendrappaE-evolvus <nagendrappa.e@evolvussolutions.com>
2024-02-23 17:58:42 +0530

    oracle and h2 db or mssql db of primary and seconf db

---
+++ springBoot3Jpa2Database/src/main/java/com/ece/springBoot3Jpa2Database/SpringBoot3Jpa2DatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.ece.springBoot3Jpa2Database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3Jpa2DatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3Jpa2DatabaseApplication.class, args);
+	}
+
+}
---
+++ springBoot3Jpa2Database/src/test/java/com/ece/springBoot3Jpa2Database/SpringBoot3Jpa2DatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ece.springBoot3Jpa2Database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3Jpa2DatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
